7079b4b99b22ca5ef3ad1a9b06ecd79129df7698
r6915ee <astergthefourth@gmail.com>
2025-10-12 23:02:34 -0700

    fix: check if data is table in component method

--- src/handecs.lua
+++ src/handecs.lua
@@ -6,11 +6,14 @@ local handecs = {
 
 --- Creates a component from the specified table.
 --- @param data table A table containing the keys and default values of the component.
---- @return number # The index of the component.
+--- @return number # The index of the component, or `0` if the data is not a table.
 function handecs:component(data)
-   local index = #self.components + 1
-   self.components[index] = data
-   return index
+   if type(data) == "table" then
+      local index = #self.components + 1
+      self.components[index] = data
+      return index
+   end
+   return 0
 end
 
 return handecs
