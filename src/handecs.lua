e56615876e3cf00baadf58bc33dd9c53ede4d852
r6915ee <astergthefourth@gmail.com>
2025-10-12 21:22:15 -0700

    docs: fix component method return annotation

--- src/handecs.lua
+++ src/handecs.lua
@@ -6,7 +6,7 @@ local handecs = {
 
 --- Creates a component from the specified table.
 --- @param data table A table containing the keys and default values of the component.
---- @return number The index of the component.
+--- @return number # The index of the component.
 function handecs:component(data)
    local index = #self.components + 1
    self.components[index] = data
