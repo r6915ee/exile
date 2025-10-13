d32c753c197730bca3a3dbed7af4fa3fda9b8bbd
r6915ee <astergthefourth@gmail.com>
2025-10-12 19:30:27 -0700

    docs: improve handecs documentation

--- handecs.lua
+++ handecs.lua
@@ -1,7 +1,12 @@
+--- @class handecs
+--- @field components table Defines all components.
 local handecs = {
    components = {},
 }
 
+--- Creates a component from the specified table.
+--- @param data table A table containing the keys and default values of the component.
+--- @return number The index of the component.
 function handecs:component(data)
    local index = #self.components + 1
    self.components[index] = data
