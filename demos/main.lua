c5d6265acf345736f3198ae2fcb065626e15661f
r6915ee <astergthefourth@gmail.com>
2025-12-25 17:39:00 -0800

    refactor: update main demo to use operate method

--- demos/main.lua
+++ demos/main.lua
@@ -3,9 +3,7 @@ local position = exile:component({ x = 0, y = 0 })
 local display = exile:component()
 
 local main = exile:schedule(function()
-   for _, entity in pairs(exile:query(position, display)) do
-      print(entity[position].x)
-   end
+   exile:operate(function(_, entity) print(entity[position].x) end, 1, 2)
 end)
 
 exile:entity(exile:mutate(position, { x = 10 }), display)
